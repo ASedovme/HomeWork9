4d88ddef6b78407fdd4292c3088cca2d73dd1f88
Aleksey Sedov <alexejsedow@gmail.com>
2024-07-31 11:02:08 +0300

    HW

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,51 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Домашнее задание 9");
+//Задача 1
+        System.out.println("Задача 1");
+        int[] costs = new int[5];
+        costs[0] = 100;
+        costs[1] = 125;
+        costs[2] = 714;
+        costs[3] = 452;
+        costs[4] = 624;
+        int allcosts = 0;
+        int i;
+        for (i = 0; i < costs.length; i++) {
+            allcosts += costs[i];
+        }
+        System.out.println("Сумма трат за месяц составила " + allcosts + " рублей");
+
+//Задача 2
+        System.out.println("Задача 2");
+        int maxCosts = -1;
+        for (i = 0; i < costs.length; i++) {
+            if (costs[i] > maxCosts) {
+                maxCosts = costs[i];
+            }
+        }
+        System.out.println("Максимальная трата за месяц " + maxCosts + " рублей");
+        int minCosts = Arrays.stream(costs).min().getAsInt();
+        ;
+        System.out.println("Минимальная трата за месяц " + minCosts + " рублей");
+//Задача 3
+        System.out.println("Задача 3");
+        double all = 0;
+        double mean = 0;
+        for (i = 0; i < costs.length; i++) {
+            all += costs[i];
+            ;
+        }
+        mean = all / i;
+        System.out.println("Средняя трата за неделю " + mean + " рублей");
+
+//Задача 4
+        System.out.println("Задача 4");
+        char[] reverseFullName = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        for (i = reverseFullName.length - 1; i >= 0; i--) {
+            System.out.print(reverseFullName[i]);
+        }
     }
 }
